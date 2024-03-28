97bfa88ebdfd897146b6707fe9520ee18d2294ec
carnage1502 <shivamsanjay152@gmail.com>
2024-03-28 11:38:20 +0530

    complete header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,14 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import React from "react";
 
-const page = () => {
-  return <div>hello</div>;
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 };
 
-export default page;
+export default Home;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section>
+      <h1>Everything is better with a Pizza!</h1>
+      <p>
+        Pizza is the missing piece that makes every day complete, a simple yet
+        delicious joy in life.
+      </p>
+      <Image src={""} alt="pizza" />
+    </section>
+  );
+};
+
+export default Hero;
